1eccf5da585e1046f138ce75d30beb2938b1e42c
spencerhh <spencer.hall16@gmail.com>
2018-12-16 02:34:01 -0800

    Fully, improved some formatting, added helper functions, but still need to make more efficient and break down into smaller functions.

--- index.js
+++ index.js
@@ -2,21 +2,37 @@
 
     "use strict";
 
-    // Constants for parsing through an individual wrestler's data
+    // Constants to help during parsing of an athlete's data
     const YEARS = ["FRESHMAN", "SOPHOMORE", "JUNIOR", "SENIOR"];
-    const SECTIONS = ["SCVAL", "TCAL", "WCAL", "BVAL", "PAL", "SCCAL", "MBL", "MTAL", "CENTRAL COAST"];
+    const SECTIONS = ["SCVAL", "TCAL", "WCAL", "BVAL", "PAL", "SCCAL", "MBL",
+        "MTAL", "CENTRAL COAST"
+    ];
 
-    // Array to hold the finished wrestler objects
-    let wrestlers = [];
+    let wrestlers = []; // Array to hold the finished wrestler objects
 
     window.addEventListener("load", initialize);
 
 
+    /**
+     * Returns the value of an element with a specified id.
+     * @param {string} id - element ID
+     * @returns {string} value of the element with the specified id
+     */
     function initialize() {
-        sendRequest(126);
+        id("submit").addEventListener("click", function() {
+            let weight = id("weight-class").value;
+            if (weight != "") {
+                sendRequest(weight);
+            }
+        });
     }
 
 
+    /**
+     * Returns the value of an element with a specified id.
+     * @param {string} id - element ID
+     * @returns {string} value of the element with the specified id
+     */
     function sendRequest(weightClass) {
         let athleteData = [];
 
@@ -34,15 +50,18 @@
     }
 
 
+    /**
+     * Returns the value of an element with a specified id.
+     * @param {string} id - element ID
+     * @returns {string} value of the element with the specified id
+     */
     function parseData(data) {
         for (let i = 1; i < data.length; i++) {
             let line = data[i];
             let wrestler = new Object();
 
-
             // RANK
             wrestler.rank = i;
-
 
             // LAST RANK
             let temp = line.indexOf(" "); // the space between the lwrank and name
@@ -50,10 +69,8 @@
             if (line.substring(temp + 1, temp + 2) == "(") {
                 temp += 6;
             }
-
             wrestler.lwrank = line.substring(0, temp);
             line = line.slice(temp);
-
 
             // YEAR
             temp = -1;
@@ -64,14 +81,12 @@
                 }
             });
 
-
             // NAME
             wrestler.name = line.substring(0, temp).trim();
 
             line = line.slice(temp);
             temp = line.indexOf(" ");
             line = line.slice(temp).trim();
-
 
             // SECTION
             temp = -1;
@@ -82,10 +97,8 @@
                 }
             });
 
-
             // SCHOOL
             wrestler.school = line.substring(0, line.indexOf(wrestler.section)).trim();
-
 
             line = line.slice(temp);
             temp = line.indexOf(" ");
@@ -95,11 +108,9 @@
             }
             line = line.slice(temp).trim();
 
-
             if (line.indexOf("Others") != -1) {
                 line = line.slice(0, line.indexOf("Others")).trim();
             }
-
 
             if (line.indexOf("LEAGUE") != -1 || line.indexOf("NATIONALS") != -1) {
                 let prevresults = []
@@ -129,13 +140,8 @@
                 wrestler.prevresults = null;
             }
 
-            //console.log(line);
-
-
-
             if (line != "") {
                 let results = parseResults(line);
-
                 if (results == null) {
                     wrestler.results = null;
                 } else {
@@ -143,36 +149,37 @@
                     wrestler.results = results;
                 }
 
-
-
-
-                let capture = line.split(/(\d-\d|\d\d-\d\d|\d\d-\d|\d-\d\d|fall|\d:\d\d)/g);
-
-
-
+                let hh = [];
+                if (line != "") {
+                    let capture = line.split(/(\d-\d|\d\d-\d\d|\d\d-\d|\d-\d\d|fall|\d:\d\d)/g);
+                    for (let i = 0; i < capture.length - 1; i += 2) {
+                        let text = capture[i] + capture[i + 1];
+                        text = text.trim();
+                        hh.push(text);
+                    }
+                    wrestler.hh = hh;
+                } else {
+                    wrestler.hh = null;
+                }
             }
-
-
-
-
             wrestlers.push(wrestler);
         }
-
         console.log(wrestlers);
         return wrestlers;
     }
 
 
-
-
+    /**
+     * Returns the value of an element with a specified id.
+     * @param {string} id - element ID
+     * @returns {string} value of the element with the specified id
+     */
     function parseResults(line) {
         // It will either be a number from 0-10 or Cons.
         if (/\d/g.test(line.substring(0, 1)) || line.substring(0, 5) == "Cons.") { // Placed in something
-
             let results = [];
 
             while (/\d/g.test(line.substring(0, 1)) || line.substring(0, 5) == "Cons.") {
-
                 let temp = line.indexOf(")");
 
                 if (line.indexOf("RULE") != -1) {
@@ -182,17 +189,11 @@
                 }
 
                 let result = line.substring(0, temp + 1);
-
                 results.push(result);
                 line = line.slice(temp + 1).trim();
-
             }
 
-            if (line != "") {
-                results.push(line);
-            }
-
-
+            results.push(line);
             return results;
         } else {
             return null;
@@ -200,51 +201,42 @@
     }
 
 
-
-
-    /*function parseHeadHead(line) {
-        // It will not star with League or Nationals
-
-        if (line.substring(0, 6) != "LEAGUE" && line.substring(0, 6) != "NATION") { // Placed in something
-            let hh = [];
-
-
-            //while (line.substring(0, 3) != "LEA" && line.substring(0, 3) != "NAT" && line.substring(0, 3) != "Oth" && line.length > 5) {
-                let temp = line.indexOf(")");
-
-
-
-                let realTemp = temp;
-                let tempLine = line.slice(temp + 2);
-
-                temp = tempLine.indexOf(" ");
-                realTemp += temp;
-
-                hh.push(line.substring(0, realTemp + 2).trim());
-                console.log(line.substring(0, realTemp + 2).trim());
-                line = line.slice(realTemp + 2).trim();
-
-
-
-
-            //}
-
-            return hh;
-        } else {
-            return null;
-        }
-    }*/
-
-
-
-
+    /**
+     * Returns the value of an element with a specified id.
+     * @param {string} id - element ID
+     * @returns {string} value of the element with the specified id
+     */
     function populatePage(wrestlers) {
         wrestlers.forEach(function(wrestler) {
-            let div = document.createElement("div");
-            div.innerHTML = wrestler.rank + " " + wrestler.lwrank + " " + wrestler.name + " " + wrestler.year + " " + wrestler.school;
-            document.getElementById("content").append(div);
+            let div = ce("div");
+            div.id = "content-child";
+            let p = ce("p");
+            p.innerHTML = wrestler.rank + " " + wrestler.lwrank + " " + wrestler.name + " " + wrestler.year + " " + wrestler.school + " " + wrestler.section + " " + wrestler.results + " " + wrestler.hh + " " + wrestler.prevresults;
+            div.append(p);
+            id("content-area").append(div);
         });
     }
 
+
+    /* ------------------------------ Helper Functions ------------------------------ */
+
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} id - element ID
+     * @returns {object} DOM object associated with id.
+     */
+    function id(id) {
+        return document.getElementById(id);
+    }
+
+
+    /**
+     * Creates and returns a DOM object with the specified tag.
+     * @param {string} el - tag of the DOM object to create
+     * @returns {DOM object} a newly created DOM object with the given tag.
+     */
+    function ce(el) {
+        return document.createElement(el);
+    }
 
 })();
