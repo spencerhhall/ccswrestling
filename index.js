999edeb89dab3157d6b66b8fb2eebf6a50113e84
spencerhh <spencer.hall16@gmail.com>
2018-12-15 19:33:27 -0800

    Beginning to parse lines themselves for data.

--- index.js
+++ index.js
@@ -3,6 +3,28 @@
      *
      */
 
+    const YEARS = ["FRESHMAN", "SOPHOMORE", "JUNIOR", "SENIOR"];
+    const SECTIONS = ["SCVAL", "TCAL", "WCAL", "BVAL", "PAL", "SCCAL", "MBL", "MTAL"];
+    const SCHOOLS = ["Alisal", "Edward Alvarez", "Andrew Hill", "Aptos", "Aragon",
+        "Archbishop Mitty", "Archbishop Riordan", "Bellarmine Prep", "Branham",
+        "Burlingame", "Capuchino", "Carmel", "Cupertino", "Del Mar", "El Camino",
+        "Evergreen Valley", "Fremont", "Gilroy", "Gonzales", "Greenfield", "Gunderson",
+        "Gunn", "Half Moon Bay", "Harbor", "Harker", "Hillsdale", "Homestead", "Independence",
+        "James Lick", "King City", "The King’s Academy", "Leigh", "Leland", "Lincoln",
+        "Live Oak", "Los Altos", "Los Gatos", "Lynbrook", "Menlo-Atherton", "Mills",
+        "Milpitas", "Monta Vista", "Monterey", "Mt. Pleasant", "North Monterey Cty.",
+        "North Salinas", "Oak Grove", "Oceana", "Overfelt", "Pacific Grove", "Pajaro Valley",
+        "Palma", "Palo Alto", "Piedmont Hills", "Pioneer", "Prospect", "Sacred Heart Cathedral",
+        "Salinas", "San Benito", "San Jose Academy", "San Lorenzo Valley", "San Mateo",
+        "Santa Clara", "Santa Cruz", "Santa Teresa", "Saratoga", "Scotts Valley", "Seaside",
+        "Sequoia", "Junipero Serra", "Silver Creek", "Ann Sobrato", "Soledad", "Soquel",
+        "South San Francisco", "St. Francis", "Terra Nova", "Valley Christian", "Watsonville",
+        "Westmont", "Wilcox", "Willow Glen", "Woodside", "Yerba Buena", ""
+    ];
+
+
+    let wrestlers = [];
+
 
     "use strict";
 
@@ -14,17 +36,87 @@
         let lines = [];
 
         $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://www.ccsrank.com/CCSDATA113.htm') + '&callback=?', function(data) {
-            console.log(data);
-            let goodData = $(data.contents).text().replace(/(<([^>]+)>)/ig, ""); // Removes html tags
-            goodData = goodData.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, ""); // Trims whitespace
-            goodData = goodData.replace(/[\n\r]+/g, " "); // Removes line breaks
-            goodData = goodData.replace(/\s{2,10}/g, " "); // Removes more than 2 spaces
-            goodData = goodData.replace(/[^\x00-\x7F]/g, ""); // Removes special characters
+            let athleteData = $(data.contents).text().replace(/(<([^>]+)>)/ig, ""); // Removes html tags
+            athleteData = athleteData.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, ""); // Trims whitespace
+            athleteData = athleteData.replace(/[\n\r]+/g, " "); // Removes line breaks
+            athleteData = athleteData.replace(/\s{2,10}/g, " "); // Removes more than 2 spaces
+            athleteData = athleteData.replace(/[^\x00-\x7F]/g, ""); // Removes special characters
+
+
             // Breaks the data into lines about each wrestler
-            lines = goodData.split(/ \d \d | - - | - \d | \d - | \d\d \d\d | \d \d\d | \d\d \d | - \d\d | \d\d - /);
-            console.log(lines);
+            athleteData = athleteData.split(/  \d |  \d\d |  - /);
+
+
+            // Order of data in athletes' lines (r- always present, n- not always)
+            // last week's rank (r) | name (r) | year (r) | school (r) | section (r) | 18-19 results (n) | head-head (n) | previous results (n)
+            // r | r | r | r | r | n | n | n
+            // Ways to identify
+            // last week's rank: first character or two in the string
+            // name: 2-3 tokens
+            // year: all caps, only 4 options
+            // school: limited options, 1-4 tokens
+            // section: 7 options, 1 token
+            // These last few are tricky because they may not even exist
+            // 18-19 results: starts with number of "Cons."
+            // head-head: 2-4 characters to start (all caps)
+            // previous results: all caps with limited options
+
+
+
+
+
+            for(let i = 1; i < athleteData.length; i++) {
+              let line = athleteData[i];
+
+              let wrestler = new Object();
+
+              wrestler.rank = i;
+
+
+              let temp = line.indexOf(" "); // the space between the lwrank and name
+
+              wrestler.lwrank = line.substring(0,temp);
+
+              line = line.slice(temp);
+
+
+
+              temp = -1;
+
+              YEARS.forEach(function(year) {
+                if(line.indexOf(year) != -1) {
+                  temp = line.indexOf(year);
+                  wrestler.year = year;
+                }
+              });
+
+              
+
+
+              wrestler.name = line.substring(0, temp);
+
+
+
+              temp = line.lastIndexOf(wrestler.year);
+              line = line.slice(0, temp);
+
+
+              //wrestler.school = ;
+              //wrestler.section = ;
+
+
+
+              wrestlers.push(wrestler);
+
+
+
+            }
+
+
+            console.log(athleteData);
+            console.log(wrestlers);
         });
-  
+
     }
 
 
